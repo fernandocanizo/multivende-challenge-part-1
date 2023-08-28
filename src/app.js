d36f1f76d6847a9a89b3458f54829fdf09ade622
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2023-08-28 15:31:42 -0400

    Add GET `/no-address-person` endpoint
    Return ordered list of persons with no address as JSON.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const httpLogger = require('morgan')
 
+const { getNoAddressPersonList, qsortPersonsByName } = require('./lib')
+
 const app = express()
 const port = 3000
 
 app.use(httpLogger('dev'))
 
-app.get('/ping', (req, res) => {
+app.get('/ping', (_, res) => {
   return res.status(200).end()
+})
+
+app.get('/no-address-person', (_, res) => {
+  return res.json(qsortPersonsByName(getNoAddressPersonList()))
 })
 
 app.listen(port, () => {
