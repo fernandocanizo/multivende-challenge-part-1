5b49e9c89fca74dc62de3088308d4573b07cd4ba
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2023-08-28 15:41:41 -0400

    Trivial: clean TABS, use spaces

--- src/lib.js
+++ src/lib.js
@@ -1,17 +1,17 @@
 
 const getNoAddressPersonList = () => {
-	const personList = require('../persons.json')
-	
-	// don't use Array.filter cause it produces a shallow copy (copy by reference)
-	const noAddressPersonList = []
+  const personList = require('../persons.json')
+  
+  // don't use Array.filter cause it produces a shallow copy (copy by reference)
+  const noAddressPersonList = []
 
-	personList.forEach(person => {
-		if (person.address === null || person.address === undefined || person.address?.length === 0) {
-			noAddressPersonList.push(structuredClone(person))
-		}
-	})
+  personList.forEach(person => {
+    if (person.address === null || person.address === undefined || person.address?.length === 0) {
+      noAddressPersonList.push(structuredClone(person))
+    }
+  })
 
-	return noAddressPersonList
+  return noAddressPersonList
 }
 
 const qsortPersonsByName = (personList) => {
@@ -37,6 +37,6 @@ const qsortPersonsByName = (personList) => {
 }
 
 module.exports = {
-	getNoAddressPersonList,
-	qsortPersonsByName,
+  getNoAddressPersonList,
+  qsortPersonsByName,
 }
