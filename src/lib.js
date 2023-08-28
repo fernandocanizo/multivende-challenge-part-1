2766d968a027013da9b91d5b190c93fba48cd5ea
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2023-08-28 14:04:17 -0400

    Solve 1.b: sort no-address-persons by name, ascending

--- src/lib.js
+++ src/lib.js
@@ -14,6 +14,29 @@ const getNoAddressPersonList = () => {
 	return noAddressPersonList
 }
 
+const qsortPersonsByName = (personList) => {
+  if (personList.length <= 1) {
+    return personList
+  }
+
+  const pivot = personList[0]
+  const pivotName = pivot.name.trim().toLowerCase()
+  const left = []
+  const rigth = []
+
+  for (let i = 1; i < personList.length; i++) {
+    const personName = personList[i].name.trim().toLowerCase()
+    if (personName < pivotName) {
+      left.push(personList[i])
+    } else {
+      rigth.push(personList[i])
+    }
+  }
+
+  return [...qsortPersonsByName(left), pivot, ...qsortPersonsByName(rigth)]
+}
+
 module.exports = {
 	getNoAddressPersonList,
+	qsortPersonsByName,
 }
